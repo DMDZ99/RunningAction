6fc93d2424cc93945c317cd8c4acc94cf690fffb
KangIngu <rielkang99@gmail.com>
2025-07-30 11:57:21 +0900

    기존 플래피 버드 게임 마스터 이식

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static GameManager gameManager;
+
+    public static GameManager Instance
     {
-        
+        get { return gameManager; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private int currentScore = 0;
+    UIManager uiManager;
+    public UIManager UIManager
     {
-        
+        get { return uiManager; }
     }
+
+    private void Awake()
+    {
+        gameManager = this;
+    }
+    private void Start()
+    {
+        //uiManager.UpdateScore(0);//점수 초기화 로직
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+        //uiManager.SetRestart();//게임 재시작 로직
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
+        //uiManager.UpdateScore(currentScore);//점수증가로직
+        Debug.Log("Score: " + currentScore);
+    }
+
 }
+
