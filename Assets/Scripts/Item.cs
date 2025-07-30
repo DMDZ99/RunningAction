b19ef71b281dec65fba364c3255c737bddb8c8c4
DMDZ99 <ldhdo430@naver.com>
2025-07-30 17:09:42 +0900

    아이템스크립트기초틀

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,19 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum ItemType
+{
+    Rush,
+    Shield,
+    Magnet,
+    Potion,
+    Coin
+}
+
 public class Item : MonoBehaviour
 {
-
-    public enum ItemType
-    {
-        Dash,
-        Shield,
-        Heal,
-        Magnet,
-        Coin
-    }
-
     public ItemType type;
 
-    public bool isDashing;
+    private void OnTriggerEnter2D(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;        // 플레이어가 아니면 리턴
+
+        ApplyEffect(other.gameObject);                  // 아이템 효과적용
+        Destroy(gameObject);                            // 아이템은 충돌후 파괴
+    }
+
+    private void ApplyEffect(GameObject player)
+    {
+        switch (type)
+        {
+            case ItemType.Rush:
+                StartCoroutine(RushEffect(player));
+                break;
+            case ItemType.Shield:
+                StartCoroutine(ShieldEffect(player));
+                break;
+            case ItemType.Magnet:
+                StartCoroutine(MagnetEffect(player));
+                break;
+            case ItemType.Potion:
+                HealPlayer(player);
+                break;
+            case ItemType.Coin:
+                GetCoin(player);
+                break;
+        }
+    }
+
+    private System.Collections.IEnumerator RushEffect(GameObject player)    // 코루틴으로 처리 : 지속시간
+    {
+        Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
+        if (rb2d == null)
+            yield break;
+
+        float originalSpeed = rb2d.velocity.x;
+        rb2d.velocity = new Vector2(30, rb2d.velocity.y);   // 속도 임의 30증가
+
+        // 질주시 무적 (3초?)
+
+        // 질주 종료시 1초간 무적
+
+        
+    }
+
+    private System.Collections.IEnumerator ShieldEffect(GameObject player)
+    {
+        // 보호막 적용 = 충돌전까지
+        // 충돌시 1초간 무적
+        // 중첩불가? 가능?
+    }
+
+    private System.Collections.IEnumerator MagnetEffect(GameObject player)
+    {
+        // 자석 효과 적용 (5초?)
+    }
+
+    private void HealPlayer(GameObject player)
+    {
+        // 플레이어의 체력 일정 회복 (최대가 100이면 40정도?)
+        // 플레이어 체력 상한선 넘지않게
+    }
+
+    private void GetCoin(GameObject player)
+    {
+        //금화는 50
+        //은화는 10
+        //동화는 1
+    }
 }
