90fe6b5718c8d073b22f40746fc336fb48696e75
DMDZ99 <ldhdo430@naver.com>
2025-07-31 21:00:59 +0900

    장애물반복생성초당

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -11,9 +11,13 @@ public class Obstacle : MonoBehaviour
     private Vector3 lastPosition;
     private ObstacleKind lastKind = ObstacleKind.Jump;  // 마지막장애물 종류 = 점프
 
+    [SerializeField] private float spawnCooldown = 0.5f;  // 장애물생성쿨타임
+    private float spawnObstacleTimer = 0f;
+
     [SerializeField] private CoinPlacer coinPlacer;     // connect CoinPlacer
 
     [SerializeField] private Transform player;
+    //[SerializeField] private float destroyDistance = 30f;   // 지나가면 파괴되게
 
     private void Start()
     {
@@ -27,10 +31,13 @@ public class Obstacle : MonoBehaviour
 
     private void Update()
     {
-        if (Vector3.Distance(player.position, lastPosition) < 20f)      // 마지막 장애물위치가 20보다줄어들면 장애물 생성 반복
+        spawnObstacleTimer += Time.deltaTime;
+
+        if (spawnObstacleTimer < spawnCooldown)
             return;
 
         SpawnObstacle();
+        spawnObstacleTimer = 0f;
     }
 
 
@@ -52,10 +59,16 @@ public class Obstacle : MonoBehaviour
 
         if (lastKind == ObstacleKind.Slide && data.kind == ObstacleKind.Slide)
         {
-            gap = Mathf.Max(1f, gap); // 0으로했더니 슬라이드장애물이 겹침 -> 최소 거리
+            gap = Mathf.Max(3f, gap); // 0으로했더니 슬라이드장애물이 겹침 -> 최소 거리
         }
 
-        Vector3 prefabLocalY = prefab.transform.position;
+        if (lastKind == ObstacleKind.Slide && data.kind == ObstacleKind.Jump)
+        {
+            gap = Mathf.Min(10f, gap); // 슬라이드 뒤에 점프나올때 간격 더 벌리기
+        }
+
+
+            Vector3 prefabLocalY = prefab.transform.position;
         Vector3 position = lastPosition + new Vector3(gap, 0f, 0f); // 최종위치계산
         position.y = prefabLocalY.y;    // 프리팹의 y좌표 사용
 
